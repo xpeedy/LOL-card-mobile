d360d43f6f37590055a2ddfb5260199fa2745370
Hairo Garcia <69333686+xpeedy@users.noreply.github.com>
2021-08-09 21:22:22 -0400

    adds components and redux global store

---
+++ store/configureStore.js
@@ -0,0 +1,12 @@
+import { combineReducers, createStore } from "redux";
+import countReducer from "../reducers/countReducer";
+
+const rootReducer = combineReducers(
+    { count: countReducer }
+);
+
+const configureStore = () => {
+    return createStore(rootReducer);
+}
+
+export default configureStore;
